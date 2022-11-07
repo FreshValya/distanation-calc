76d029d6ac8986a6530f62eb89a366c7c678f279
Валерий Трубин <Your new email>
2022-11-08 00:20:23 +0400

    init project

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+const App = () => {
+  return <h1>BREAKING APP</h1>;
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index',
+  mode: 'development',
+
+  output: {
+    publicPath: '/',
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
+
+  devtool: 'eval-source-map',
+  devServer: {
+    hot: true,
+  },
+
+  resolve: {
+    modules: [__dirname, 'src', 'node_modules'],
+    extensions: ['*', '.js', '.jsx', '.tsx', '.ts'],
+  },
+
+  module: {
+    rules: [
+      {
+        test: /.(js|ts)x?$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                localIdentName: '[local]--[hash:base64:5]',
+              },
+            },
+          },
+          {
+            loader: 'sass-loader',
+          },
+        ],
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+};
